a96efe3fd2d77d4a1cd401e0700edec301dfa0a1
Jonny Waern <jonnywaern@hotmail.com>
2022-06-13 18:41:18 +0200

    gubbe

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/gubbe/i);
+  const linkElement = screen.getByText(gubbe);
   expect(linkElement).toBeInTheDocument();
 });
